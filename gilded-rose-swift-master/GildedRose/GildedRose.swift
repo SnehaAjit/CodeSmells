a61a032092ef50a05a86235339a6241dd4e02b06
Ajit, Sneha <sneha.ajit@sap.com>
2018-04-25 12:44:41 +0530

    Fourth commit

--- gilded-rose-swift-master/GildedRose/GildedRose.swift
+++ gilded-rose-swift-master/GildedRose/GildedRose.swift
@@ -24,67 +24,58 @@ public class GildedRose {
     }
     
     public static func updateAgedBrieItem( item:inout Item) -> Item {
-        var item = item
-        if item.quality < 50 {
+        if item.sellIn <= 0 {
+                item.quality = item.quality + 2
+        } else {
             item.quality = item.quality + 1
         }
-        item.sellIn = item.sellIn - 1
-        if item.sellIn < 0 {
-            if item.quality < 50 {
-                item.quality = item.quality + 1
-            }
+        if item.quality > 50 {
+            item.quality = 50
         }
+        item.sellIn = item.sellIn - 1
         return item
     }
     
     public static func updateBackstageItem( item:inout Item) -> Item {
-        if item.quality < 50 {
+        if(item.sellIn <= 0) {
+            item.quality = 0
+        }else if item.sellIn < 6 {
+            item.quality = item.quality + 3
+        } else if item.sellIn < 11 {
+            item.quality = item.quality + 2
+        }else {
             item.quality = item.quality + 1
-            if item.sellIn < 11 {
-                if item.quality < 50 {
-                    item.quality = item.quality + 1
-                }
-            }
-            if item.sellIn < 6 {
-                if item.quality < 50 {
-                    item.quality = item.quality + 1
-                }
-            }
+        }
+        if(item.quality > 50) {
+            item.quality = 50
         }
         item.sellIn = item.sellIn - 1
-        if item.sellIn < 0 {
-            item.quality = item.quality - item.quality
-        }
         return item
     }
     
     public static func updateGeneralItem( item:inout Item) -> Item {
-        if item.quality > 0 {
+        if item.sellIn <= 0 {
+            item.quality = item.quality - 2
+        } else {
             item.quality = item.quality - 1
         }
-        item.sellIn = item.sellIn - 1
-        if item.sellIn < 0 {
-            if item.quality > 0 {
-                item.quality = item.quality - 1
-            }
+        if(item.quality < 0) {
+            item.quality = 0
         }
+        item.sellIn = item.sellIn - 1
         return item
     }
     
     public static func updateConjuredItem( item:inout Item) -> Item {
-        if item.quality > 1 {
-            item.quality = item.quality - 2
+        if item.sellIn <= 0 {
+            item.quality = item.quality - 4
         } else {
+            item.quality = item.quality - 2
+        }
+        if(item.quality < 0) {
             item.quality = 0
         }
         item.sellIn = item.sellIn - 1
-        if item.sellIn < 0 {
-            if item.quality > 1 {
-                item.quality = item.quality - 2
-            } else {
-                item.quality = 0
-            }
-        }
         return item
     }
 }
