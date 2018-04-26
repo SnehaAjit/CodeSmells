13031e9b7e74f1d372f5fc97bc64a183001cc0b5
Pajaniraja, Manjula <manjula.pajaniraja@sap.com>
2018-04-26 12:12:28 +0530

    Removing if-else

--- gilded-rose-swift-master/GildedRose/GildedRose.swift
+++ gilded-rose-swift-master/GildedRose/GildedRose.swift
@@ -17,54 +17,39 @@ public class GildedRose {
                 case Constants.conjuredItem : updateConjuredItem(item: &item)
                 default : updateGeneralItem(item: &item)
             }
-            if item.name != Constants.sulfuras {
-              updateSellinForItem(item: &item)
-            }
             updatedItems.append(item)
         }
         return updatedItems
     }
     
     public static func updateAgedBrieItem( item:inout Item){
-        if item.sellIn <= 0 {
-            updateItemQuality(item: &item, factor: 2)
-        } else {
-            updateItemQuality(item: &item, factor: 1)
-        }
+        item.sellIn <= 0 ?  updateItemQualityAndSellin(item: &item, factor: 2) : updateItemQualityAndSellin(item: &item, factor: 1)
     }
     
     public static func updateBackstageItem( item:inout Item) {
         if(item.sellIn <= 0) {
             item.quality = 0
+            updateSellinForItem(item: &item)
         }else if item.sellIn < 6 {
-            updateItemQuality(item: &item, factor: 3)
+            updateItemQualityAndSellin(item: &item, factor: 3)
         } else if item.sellIn < 11 {
-            updateItemQuality(item: &item, factor: 2)
+            updateItemQualityAndSellin(item: &item, factor: 2)
         }else {
-            updateItemQuality(item: &item, factor: 1)
+            updateItemQualityAndSellin(item: &item, factor: 1)
         }
     }
     
     public static func updateGeneralItem( item:inout Item) {
         if(item.name != Constants.sulfuras) {
-            if item.sellIn <= 0 {
-                updateItemQuality(item: &item, factor: -2)
-            } else {
-                updateItemQuality(item: &item, factor: -1)
-            }
+            item.sellIn <= 0 ?  updateItemQualityAndSellin(item: &item, factor: -2) :  updateItemQualityAndSellin(item: &item, factor: -1)
         }
     }
     
     public static func updateConjuredItem( item:inout Item) {
-        if item.sellIn <= 0 {
-           updateItemQuality(item: &item, factor: -4)
-        } else {
-           updateItemQuality(item: &item, factor: -2)
-        }
-      
+         item.sellIn <= 0 ?  updateItemQualityAndSellin(item: &item, factor: -4) :  updateItemQualityAndSellin(item: &item, factor: -2)
     }
     
-    public static func updateItemQuality( item:inout Item, factor: Int) {
+    public static func updateItemQualityAndSellin( item:inout Item, factor: Int) {
         item.quality = item.quality + factor
         if(item.quality < 0) {
             item.quality = 0
@@ -72,6 +57,7 @@ public class GildedRose {
         if(item.quality > 50) {
             item.quality = 50
         }
+        updateSellinForItem(item: &item)
     }
     
     public static func updateSellinForItem(item:inout Item) {
